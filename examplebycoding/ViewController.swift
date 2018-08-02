24e0eaaa8523fd5f311c9cbe0695f08db55430df
jason <876999351@qq.com>
2018-08-02 13:50:02 -0700

    test2 version

--- examplebycoding/ViewController.swift
+++ examplebycoding/ViewController.swift
@@ -10,17 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let aView : UIImageView = {
+        let img = UIImageView(image: #imageLiteral(resourceName: "icon"))
+        img.translatesAutoresizingMaskIntoConstraints = false
+        return img
+    }()
+    
+    
+    let bView: UIImageView = {
+        let img = UIImageView(image: #imageLiteral(resourceName: "cat"))
+        img.translatesAutoresizingMaskIntoConstraints = false
+        return img
+    }()
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        // set a test view
+        setFirstTestView()
        
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setFirstTestView(){
+        [aView, bView].forEach {view.addSubview($0)}
+        aView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 12), size: .init(width: 100, height: 100))
+        bView.anchor(top: aView.bottomAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 20, left: 0, bottom: 0, right : 12), size: .init(width: 100, height: 100))
+    }
+    
+    
+   
 
 }
+
+extension UIView{
+    
+    func anchorSize(to view: UIView){
+        
+        widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        if let top = top{
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading{
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom{
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing{
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        
+    }
+}
+
 
